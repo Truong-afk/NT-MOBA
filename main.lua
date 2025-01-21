6d67aca64a514427215cf8f16f39d155552caca4
Truong-afk <nntruong-cntt18@tdu.edu.vn>
2025-01-21 19:33:18 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Truong-afk/NTMOBA_2/refs/heads/main/scriptupdate.txt?token=GHSAT0AAAAAAC5RPVM7DBF7KVRFNX7J3S3IZ4PSDCA"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Truong-afk/NTSC/refs/heads/main/scriptupdate.txt"))()
